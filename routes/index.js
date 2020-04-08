612f7595921dbb6490d656862e8e134072103ae4
Prathamesh More <moreprathameshmore@hotmail.com>
2020-04-08 23:47:43 +0530

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const twilio = require('twilio');
+const axios = require('axios');
+
+const router = express.Router();
+
+const ACCOUNT_ID = process.env.ACCOUNT_ID;
+const API_KEY = process.env.API_KEY;
+
+const client = new twilio(ACCOUNT_ID, API_KEY);
+
+
+async function getCovidData() {
+  const covidINdata = await axios.get('https://api.covid19api.com/live/country/india/status/confirmed');
+
+  const latestData = covidINdata.data;
+
+  const recentData = latestData[latestData.length - 1];
+
+  return recentData;
+}
+
+/* GET home page. */
+router.get('/', async (req, res, next) => {
+
+  const latestData = await getCovidData();
+
+  res.render('index', { isSuccess: false, latestData: latestData });
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    const { mobile } = req.body;
+
+    const recentData = await getCovidData();
+
+
+    const messageBody = `Currently, ${recentData.Country} has a total of ${recentData.Confirmed} confirmed cases, ${recentData.Deaths} deaths and the good news is ${recentData.Recovered} recovered. Still ${recentData.Active} active cases. \nStay at home, stay safe!  :) \nPrathamesh`
+
+    if (mobile) {
+      client.api.messages.create({
+        body: messageBody,
+        to: '+91' + mobile,
+        from: +12569789527
+      }).then(data => {
+        console.log('SMS Sent' + data);
+        return res.status(200).render('index', { isSuccess: true, latestData: recentData });
+      }).catch(error => {
+        console.log(error);
+        return res.status(200).render('index', { isSuccess: false, latestData: recentData });
+      });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+module.exports = router;
